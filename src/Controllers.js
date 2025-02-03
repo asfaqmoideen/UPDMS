64f24ea0f1e76c55b5568bcb537bed4560bdc76f
asfaqmoideen <asfaqmoideen@gmail.com>
2025-02-03 20:20:20 +0530

    Add, remove users, with greetings

--- src/Controllers.js
+++ src/Controllers.js
@@ -1,27 +1,28 @@
 import { users, emailList } from "./data";
     export class UserController{
         constructor(){
-            this.userUI = new UIController();
-            this.currentUserId = 1;
+            this.userUI = new UIController(this);
+            this.userId = 1;
         }
 
         populateUserProfile(){
             this.userUI.setProfileValues(users.get(1))
         }
 
+        populateUsersTable(){
+            this.userUI.setUsersTable(users);
+        }
         addUser(user){
             console.log(users.set(user.id, user));
-            console.log(emailList.add(user.email));
         }
          viewAllUsers(){
             return users;
         }
 
         tryeditingProfile({firstName,lastName,age,city,street,zipcode}){
-
-            const user = users.get(this.currentUserId);
+            const user = users.get(this.userId);
             const nestedObject = {
-                id : this.currentUserId,
+                id : this.userId,
                 firstName : firstName || user.firstName,
                 lastName : lastName || user.lastName,
                 age : age || user.age,
@@ -32,17 +33,46 @@ import { users, emailList } from "./data";
                 }
             };
 
-            users.set(this.currentUserId, nestedObject);
-            this.userUI.setProfileValues(users.get(this.currentUserId));
+            users.set(this.userId, nestedObject);
         }
         
-        trySettingGreeting(){
-            this.userUI.setGreeting(users.get(this.currentUserId));
+        trySettingGreeting(userId){
+            this.userUI.setGreeting(users.get(userId));
+        }
+        tryAddingUser({firstName,lastName,age,city,street,zipcode}){
+            this.addUser({id : users.size+1 ,firstName : firstName,
+                lastName : lastName,
+                age : age,
+                city : city,
+                address :{
+                    street : street ,
+                    zipcode : zipcode 
+                }})
+           this.userUI.resetContextForAddUser();
+           this.userUI.updateTableBody();
+           this.userUI.showFormModal(false);
+        }
+        tryRemovingUser(userId){
+            users.delete(userId);
+            this.userUI.updateTableBody();
         }
     }
+    
+    class UIController{
+        constructor(userCon){
+            this.tableHeadings = [{id : "ID"}, {firstName : "First Name"}, {lastName : "Last Name"}, {age : "Age"}, {city : "City"}, {address : "Address"} , {action: "Action"}]
+            this.userCon = userCon
+        }
+        showFormModal(show){
+            const modal = document.getElementById("editmodal");
+            show ? modal.classList.remove("hidden") : modal.classList.add("hidden");
+        }
 
-class UIController{
-    constructor(){}
+    resetContextForAddUser(){
+
+        document.getElementById("profile-submit").textContent = "Edit Profile";
+    }
+
     setProfileValues(user){
         const { id, firstName, lastName, age, city, address: { street, zipcode } } = user;
         const userArray = [firstName, lastName, age, city, street, zipcode];
@@ -73,6 +103,87 @@ class UIController{
         Hello, this is ${user.firstName} ${user.lastName},
         from ${user.city}.`
     }
+
+    setUsersTable(){
+        this.setTableHeadings();
+        this.updateTableBody();
+    }
+
+    setTableHeadings(){
+        const tablehead = document.querySelector("#usersTable thead");
+        tablehead.textContent = "";
+        this.tableHeadings.forEach((heading) => {
+            const td = document.createElement("td");
+            td.textContent = Object.values(heading);
+            tablehead.appendChild(td);
+        })
+    }
+
+    updateTableBody(){
+        const tbody = document.querySelector("#usersTable tbody");
+        tbody.textContent = "";
+        users.forEach((value, key) =>{
+            const row = document.createElement("tr");
+
+            for(let x in value){
+                const td = document.createElement('td');
+                if(typeof value[x] === "object"){
+                    td.textContent = this.getToString(value[x]);
+                }
+                else{
+                td.textContent = value[x];
+                }
+                row.appendChild(td);
+            }
+
+            row.appendChild(this.createActionCell(key));
+            tbody.appendChild(row);
+        })
+
+    }
+    
+    getToString(object){
+        return Object.values(object).join(",");
+    }
+
+    createActionCell(key){
+        const actions = document.createElement("div");
+        actions.className = "actioncell";
+        actions.appendChild(this.createEditCell(key));
+        actions.appendChild(this.createRemoveCell(key));
+        actions.appendChild(this.createGreetingCell(key));
+        return actions;
+    }
+    createEditCell(userId){
+        const span = document.createElement("span");
+        span.textContent = "Edit";
+        span.className = "btnspan"
+        span.addEventListener('click', ()=>{
+            this.showFormModal(true);
+            this.userCon.userId = userId;
+            this.updateTableBody();
+        });
+        return span;
+    }
+
+    createGreetingCell(userId){
+        const span = document.createElement("span");
+        span.textContent = "Greeting";
+        span.className = "btnspan"
+        span.addEventListener('click', ()=>{
+            this.userCon.trySettingGreeting(userId)
+        });
+        return span;
+    }
+    createRemoveCell(userId){
+        const span = document.createElement("span");
+        span.textContent = "Remove";
+        span.className = "btnspan"
+        span.addEventListener('click', ()=>{
+            this.userCon.tryRemovingUser(userId)
+        });
+        return span;
+    }
 }
 
 export class AdditionalFeatures{
@@ -83,7 +194,6 @@ export class AdditionalFeatures{
 
     generateFibo(n) {
         const fibo = [...this.fibonacci(n)]; 
-        console.log(fibo);
         const x = fibo.join(", ");
     
         this.ui.setFiboOutput(x);
--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import { UserController, AdditionalFeatures } from "./Controllers";
 document.addEventListener("DOMContentLoaded", () => {
     const userCon = new UserController();
     const addfeat = new AdditionalFeatures();
-    userCon.populateUserProfile();
+    userCon.populateUsersTable();
     addfeat.getImageFromInternet();
-    
+
     document.getElementById("fibobtn").addEventListener("click", () => {
         addfeat.generateFibo(document.getElementById("fiboinput").value);
     })
@@ -28,12 +28,24 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById('greetclose').addEventListener("click", ()=>{
         document.getElementById('greetingcard').classList.add("hidden");
     })
-
+    
+    document.getElementById('addbtn').addEventListener('click', ()=>{
+        document.getElementById("editmodal").classList.remove("hidden");
+        document.getElementById("profile-submit").textContent = "Add Profile"
+        
+    })
     const form = document.getElementById('editform');
     form.addEventListener('submit', (event)=> {
         event.preventDefault();
         const formData = new FormData(form);
         const data = Object.fromEntries(formData.entries());
+
+        if(document.getElementById('profile-submit').textContent == "Add Profile"){
+            userCon.tryAddingUser(data);
+            return; 
+        }
         userCon.tryeditingProfile(data);
+        userCon.populateUsersTable();
+        document.getElementById("editmodal").classList.add("hidden");
     })
 })
